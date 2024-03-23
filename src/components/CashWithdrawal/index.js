85c9445da8686d9e90f6440862a2403e35060b23
kuldip lohare <162694586+kuldipl78@users.noreply.github.com>
2024-03-23 11:56:38 +0530

    Update index.js

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -7,19 +7,19 @@ import DenominationItem from '../DenominationItem'
 class CashWithdrawal extends Component {
   state = {totalValue: 0}
 
-  onDenominationClick = () => {
+  onDenominationClick = value => {
     this.setState(prevState => ({
-      totalValue: prevState.totalValue + this.State.totalValue,
+      totalValue: prevState.totalValue + value,
     }))
   }
 
   render() {
-    const {withdrowAmout} = this.state
+    const {withdrowAmout} = this.props
     return (
       <div className="main-Continer">
         <div className="withDraw-Container">
           <div className="head-Container">
-            <p className="headName"> S</p>
+            <h1 className="headName"> S</h1>
             <p className="name"> Sarah Williams </p>
           </div>
           <div className="balance-container">
@@ -34,11 +34,10 @@ class CashWithdrawal extends Component {
             <p className="description"> CHOOSE SUM (IN RUPEES)</p>
           </div>
           <ul className="values-Continer">
-            {denominationsList.map(eachValue => (
+            {this.props.denominationsList.map(eachValue => (
               <DenominationItem
                 key={eachValue.id}
-                eachValue={eachValue}
-                onDenominationClick={onDenominationClick}
+                onDenominationClick={this.onDenominationClick}
                 value={eachValue.value}
               />
             ))}
